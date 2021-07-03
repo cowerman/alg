91184bc74056ea306d4cc66b6ca94f9e373e9bd3
qsort987 <prj_share@sina.com>
2021-07-03 11:12:01 +0800

    fix: add pre-compile instruction to avoid multil header file nested in the target
    Signed-off-by: qsort987 <prj_share@sina.com>

--- include/lib/struct/array.h
+++ include/lib/struct/array.h
@@ -1,3 +1,6 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
 #define ARRAY_LEN 20
 struct data {
 	int random_num[ARRAY_LEN];
@@ -8,3 +11,5 @@ int array_insert(int *data, int idx, struct data *);
 int array_del_idx(int idx, struct data *);
 int array_del_ctx(int *data, struct data *);
 int array_find(int *data, struct data);
+
+#endif
